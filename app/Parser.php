1a6c972b87094d3e7120218b7cb45563344eeea7
Murilo Magalhaes <murilomagalhaes@outlook.com>
2026-02-27 18:37:53 -0300

    refactor: change class object calls to local variable; less accesses to the main url array inside the main loop

--- app/Parser.php
+++ app/Parser.php
@@ -6,22 +6,31 @@ namespace App;
 final class Parser
 {
     const string BASE_URL = 'https://stitcher.io';
-    private array $urls = [];
 
     public function parse(string $inputPath, string $outputPath): void
     {
+
         $urlPathOffset = strlen(self::BASE_URL);
 
         $inputFile = fopen($inputPath, "r");
 
         gc_disable();
 
+        $urls = [];
+
         while ($line = fgets($inputFile)) {
-            $this->processLine($line, $urlPathOffset);
+
+            $path = substr($line, $urlPathOffset, -27);
+            $dateKey = (int)str_replace('-', '', substr($line, -26, 10));
+
+            $urls[$path][$dateKey] ??= 0;
+            $urls[$path][$dateKey]++;
         }
 
-        foreach ($this->urls as &$visits) {
+        foreach ($urls as &$visits) {
+
             ksort($visits, SORT_NUMERIC);
+
             $visits = array_combine(
                 array_map(
                     fn(int $dateKey): string => preg_replace('/(\d{4})(\d{2})(\d{2})/', '$1-$2-$3', (string)$dateKey),
@@ -35,19 +44,8 @@ final class Parser
 
         fclose($inputFile);
 
-        file_put_contents($outputPath, json_encode($this->urls, JSON_PRETTY_PRINT));
+        file_put_contents($outputPath, json_encode($urls, JSON_PRETTY_PRINT));
 
-    }
-
-    private function processLine(string $line, int $urlPathOffset): void
-    {
-        $path = substr($line, $urlPathOffset, -27);
-        $date = substr($line, -26, 10);
-
-        $dateKey = (int)str_replace('-', '', $date);
-
-        $visit = &$this->urls[$path][$dateKey];
-        ++$visit;
     }
 
 }
